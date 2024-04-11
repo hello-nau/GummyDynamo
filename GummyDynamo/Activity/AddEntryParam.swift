874cb3e925e69a1b20a48281cb0dfe2d2b33ca76
Yana T <yana.work.related@gmail.com>
2024-04-10 19:32:48 -0700

    AddEntryParam activity

---
+++ GummyDynamo/Activity/AddEntryParam.swift
@@ -0,0 +1,42 @@
+//
+//  AddEntryParam.swift
+//  GummyDynamo
+//
+//  Created by ヤナ on 4/10/24.
+//
+
+import Foundation
+
+class AddEntryParam {
+    private var entry: Entry
+    
+    init(entry: Entry) {
+        self.entry = entry
+    }
+    
+    func addStatus(entryStatus: EntryStatus) {
+        var statusMap = entry.getStatusMap()
+        
+        if statusMap[entryStatus] == nil {
+            statusMap[entryStatus] = [Date().roundedToSeconds()]
+        } else {
+            var list = statusMap[entryStatus]!
+            statusMap[entryStatus]?.append(Date().roundedToSeconds())
+        }
+    }
+    
+    func addCraving(craving: String) {
+        var cravingList = entry.getCravings()
+        cravingList.append(Craving(craving: craving))
+    }
+}
+
+extension Date {
+        func roundedToSeconds() -> Date {
+            let cal = Calendar.current
+            let roundedDate = cal.date(bySetting: .second, value: 0, of: self)
+            return roundedDate ?? self
+        }
+    }
+
+
---
+++ GummyDynamo/models/Day.swift
@@ -0,0 +1,15 @@
+//
+//  Day.swift
+//  GummyDynamo
+//
+//  Created by ヤナ on 4/10/24.
+//
+
+import Foundation
+
+class Day {
+    private let date: Date
+    private var entry: Entry
+    private var addEntryParam: AddEntryParam
+    
+}
