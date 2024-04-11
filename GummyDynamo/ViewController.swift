dff1f847cc85ac8c137399094b3b562851e7adcc
Yana T <yana.work.related@gmail.com>
2024-04-10 18:01:18 -0700

    Initial Commit

---
+++ GummyDynamo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GummyDynamo
+//
+//  Created by ヤナ on 4/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
