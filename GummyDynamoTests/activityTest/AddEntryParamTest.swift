ad7c77954015663820a1a7fe35604e7d4d6c8882
Yana T <yana.work.related@gmail.com>
2024-04-11 15:17:55 -0700

    added more tests

--- GummyDynamoTests/activityTest/AddEntryParamTest.swift
+++ GummyDynamoTests/activityTest/AddEntryParamTest.swift
@@ -11,8 +11,8 @@ import XCTest
 class AddEntryParamTest: XCTestCase{
 
     func testAddStatusReturnsCorrectTime() {
-        var entry = Entry.EntryBuilder().build()
-        var addEntryParam = AddEntryParam(entry: entry)
+        let entry = Entry.EntryBuilder().build()
+        let addEntryParam = AddEntryParam(entry: entry)
         addEntryParam.addStatus(entryStatus: .ok)
         
         let entryTimeStamp = entry.getStatusMap()[.ok]
@@ -23,5 +23,57 @@ class AddEntryParamTest: XCTestCase{
         print("From test currentTime: ", currentTime)
         XCTAssertEqual(currentTime, entryTimeStamp?[0])
     }
+    func testAddStatusAddsTwoTimes(){
+        let entry = Entry.EntryBuilder().build()
+        let addEntryParam = AddEntryParam(entry: entry)
+        
+        addEntryParam.addStatus(entryStatus: .ok)
+        addEntryParam.addStatus(entryStatus: .ok)
+        
+        let entryTimestamp = entry.getStatusMap()[.ok]
+        XCTAssertEqual(entryTimestamp?.count, 2)
+    }
     
+    func testAddStatusHas2Entries() {
+        let entry = Entry.EntryBuilder().build()
+        let addEntryParam = AddEntryParam(entry: entry)
+        addEntryParam.addStatus(entryStatus: .ok)
+        addEntryParam.addStatus(entryStatus: .hungry)
+
+        let entryTimestamp = entry.getStatusMap()[.hungry]
+        let now = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: now)
+        let currentTime = calendar.date(from: components)!
+        
+        let statusMap = entry.getStatusMap()
+
+        XCTAssertEqual(statusMap.count, 2)
+     
+        XCTAssertTrue(entry.getStatusMap().keys.contains(.hungry))
+    }
+    
+    func testAddCraving() {
+        let entry = Entry.EntryBuilder().build()
+        let addEntryParam = AddEntryParam(entry: entry)
+        addEntryParam.addCraving(craving: "Gummy")
+
+        let expected = "Gummy"
+        let actual = entry.getCravings()[0].getCraving()
+
+        XCTAssertEqual(expected, actual, "Cravings should match.")
+    }
+    func testAddMultipleCraving() {
+        let entry = Entry.EntryBuilder().build()
+        let addEntryParam = AddEntryParam(entry: entry)
+        addEntryParam.addCraving(craving: "Gummy")
+        addEntryParam.addCraving(craving: "Bear")
+        addEntryParam.addCraving(craving: "Candy")
+
+        let expected = "Bear"
+        let actual = entry.getCravings()[1].getCraving()
+
+        XCTAssertEqual(expected, actual, "Cravings should match.")
+        XCTAssertEqual(3, entry.getCravings().count)
+    }
 }
