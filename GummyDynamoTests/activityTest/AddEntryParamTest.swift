96ec46f146d44a0eee9d3d9a11b19e9250178191
Yana T <yana.work.related@gmail.com>
2024-04-11 12:23:57 -0700

    AddEntryParamTest

---
+++ GummyDynamoTests/activityTest/AddEntryParamTest.swift
@@ -0,0 +1,33 @@
+//
+//  AddEntryParamTest.swift
+//  GummyDynamoTests
+//
+//  Created by ヤナ on 4/11/24.
+//
+
+import XCTest
+@testable import GummyDynamo
+
+class AddEntryParamTest: XCTestCase{
+    var entry: Entry!
+    
+    override func setUp() {
+        super.setUp()
+        entry = Entry.EntryBuilder().build()
+    }
+    
+    func testAddStatusReturnsCorrectTime() {
+        let addEntryParam = AddEntryParam(entry: entry)
+        addEntryParam.addStatus(entryStatus: .ok)
+        
+        let entryTimeStamp = entry.getStatusMap()[.ok]
+        let now = Date()
+        let calendar = Foundation.Calendar.current
+        let components = calendar.dateComponents([.year,.month, .day, .hour, .minute], from: now)
+        let currentTime = calendar.date(from: components)!
+        
+        XCTAssertEqual(currentTime, entryTimeStamp?[0])
+        
+    }
+    
+}
